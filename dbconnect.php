457518ebdde964bb86138c624f591bf5dc070081
Noah Mautner <noah.mautner@outlook.de>
2018-12-26 01:50:48 +0100

    Create dbconnect.php
    Funktioniert noch nicht

---
+++ dbconnect.php
@@ -0,0 +1,24 @@
+<?php 
+// (Server, Benutzername, PW, DBName)
+//$db = new mysql_connect("rdbms.strato.de", "U2896074","unter_SLIP_2017", "DB2896074");
+
+$dbtest = new PDO('mysql:host=rdbms.strato.de;dbname=DB2896074', "U2896074", "unter_SLIP_2017");
+
+
+if(!$dbtest){
+	exit("Verbindungsfehler: ".mysqli_connect_error());
+} else echo "string";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DBTest</title>
+</head>
+<body>
+
+
+
+</body>
+</html>
+
