d5db277159b2214bac2c629bf105b68cdbda2977
Noah Mautner <noah.mautner@outlook.de>
2018-12-27 22:40:35 +0100

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,16 @@
 <?php 
 // (Server, Benutzername, PW, DBName)
-//$db = new mysql_connect("rdbms.strato.de", "U2896074","unter_SLIP_2017", "DB2896074");
-
+$db = new mysql_connect("rdbms.strato.de", "U2896074","unter_SLIP_2017", "DB2896074");
+echo "string";
 $dbtest = new PDO('mysql:host=rdbms.strato.de;dbname=DB2896074', "U2896074", "unter_SLIP_2017");
 
 
 if(!$dbtest){
 	exit("Verbindungsfehler: ".mysqli_connect_error());
+} else echo "string";
+
+if(!$db){
+	exit("Verbindungsfehler: ".mysql_connect_error());
 } else echo "string";
 ?>
 
