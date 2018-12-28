93e5e6e651cdbeecc7287089469a454d3a690b23
Noah Mautner <noah.mautner@outlook.de>
2018-12-28 19:20:47 +0100

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,28 +1,24 @@
 <?php 
-// (Server, Benutzername, PW, DBName)
-$db = new mysql_connect("rdbms.strato.de", "U2896074","unter_SLIP_2017", "DB2896074");
-echo "string";
-$dbtest = new PDO('mysql:host=rdbms.strato.de;dbname=DB2896074', "U2896074", "unter_SLIP_2017");
+# Zugangsdaten
+$db_server = 'localhost';
+$db_benutzer = 'root';
+$db_passwort = '';
+$db_name = 'myPizza';
 
+# Verbindungsaufbau
+if(mysqli_connect($db_server, $db_benutzer, $db_passwort)) {
+echo 'Server-Verbindung erfolgreich, Datenbank erfolgreich ausgewählt.
+';
+}
+else {
+echo 'Verbindung nicht möglich, bitte Daten prüfen!
 
-if(!$dbtest){
-	exit("Verbindungsfehler: ".mysqli_connect_error());
-} else echo "string";
-
-if(!$db){
-	exit("Verbindungsfehler: ".mysql_connect_error());
-} else echo "string";
+';
+echo 'MYSQL-Fehler: '.mysql_error();
+}
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>DBTest</title>
-</head>
-<body>
 
 
 
-</body>
-</html>
 
