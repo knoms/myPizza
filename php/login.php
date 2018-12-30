241312c29c471c7fb0e36de03740a9dddaf7b7ae
Noah Mautner <noah.mautner@outlook.de>
2018-12-30 23:39:40 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,19 +1,25 @@
 <?php 
 session_start();
-$pdo = new PDO('mysql:host=localhost;dbname=myPizza', 'root', '');
+include("dbconnect.php")
+
  
 if(isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
     
-    $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
-    $result = $statement->execute(array('email' => $email));
-    $user = $statement->fetch();
+    $statement =  = mysqli_query("SELECT * FROM mp_users WHERE Email = '$email'");
+   
         
-    //Überprüfung des Passworts
-    if ($user !== false && password_verify($password, $user['password'])) {
-        $_SESSION['userid'] = $user['id'];
-        die('Login erfolgreich. Weiter zu <a href="willkommen.html">Konto</a>');
+    //Emailadresse bekannt?
+    if (mysqli_num_rows($statement)==1) {
+
+            if ($password==$statement['Pw']) {
+                header(Location: ../welcome.html);
+                
+            }
+            
+
+
     } else {
         $errorMessage = "E-Mail oder Passwort war ungültig<br>";
         header("Location: ../login.html");
