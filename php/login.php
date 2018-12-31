3c1ce822b477b0f38e00880d82b29a9c328a9734
lena0807 <40233702+lena0807@users.noreply.github.com>
2018-12-31 01:12:14 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,20 +1,20 @@
 <?php 
 session_start();
-include("dbconnect.php")
+include("dbconnect.php");
 
  
 if(isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
     
-    $statement =  = mysqli_query("SELECT * FROM mp_users WHERE Email = '$email'");
+    $statement = mysqli_query("SELECT * FROM mp_users WHERE Email = '$email'");
    
         
     //Emailadresse bekannt?
     if (mysqli_num_rows($statement)==1) {
 
             if ($password==$statement['Pw']) {
-                header(Location: ../welcome.html);
+                header("Location: ../welcome.html");
                 
             }
             
