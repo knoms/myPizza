5d0001a67e0ec1f94c2f0a5ed00b7394d33a69f4
Liana2808 <40233721+Liana2808@users.noreply.github.com>
2019-01-08 23:32:25 +0100

    Update ueberUns.php

--- ueberUns.php
+++ ueberUns.php
@@ -78,7 +78,7 @@ echo nl2br(print_r($_SESSION,true)); // Nur zu Debugzwecken, kann auskommentiert
 	</div> 
 
 	<div class="w3-container w3-center">
-	  <div class="w3-card-4 center" style="width:40%">
+	  <div class="w3-card-4 center" style="width:50%">
 	    <img src="Images/myPizza.jpeg" alt="Norway" style="width:100%">
 	    <div class="w3-container w3-center">
 	      <p>We want to change the way people eat Pizza.
@@ -89,7 +89,7 @@ echo nl2br(print_r($_SESSION,true)); // Nur zu Debugzwecken, kann auskommentiert
 
 	<div class="w3-container">
 	  <br>
-	  <div class="w3-card-4 center" style="width:40%">
+	  <div class="w3-card-4 center" style="width:50%">
 	    <img src="Images/teig-vorbereitung.jpg" alt="Norway" style="width:100%">
 	    <div class="w3-container">
 	      <h3><strong>Qualität & Frische</strong></h3>
@@ -101,7 +101,7 @@ echo nl2br(print_r($_SESSION,true)); // Nur zu Debugzwecken, kann auskommentiert
 
 	<div class="w3-container">
 		  <br>
-		  <div class="w3-card-4 center" style="width:40%">
+		  <div class="w3-card-4 center" style="width:50%">
 		    <img src="Images/zutaten.jpg" alt="Norway" style="width:100%">
 		    <div class="w3-container">
 		      <h3><strong>Regionale Produkte</strong></h3>
