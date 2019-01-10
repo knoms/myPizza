a03eaa3cb03b15d56fe500441fe3300dabfaaeac
Liana2808 <40233721+Liana2808@users.noreply.github.com>
2019-01-10 16:02:32 +0100

    Deine Bestellung

---
+++ order.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="styleSchrift.css" type="text/css" rel="stylesheet">
+
+	<title>hhh</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="w3-container w3-center">
+		<div class="w3-panel w3-border-top w3-border-bottom w3-light-green">
+  			<h2 style="color: white;">- Deine Bestellung - </h2>
+		</div>
+	</div>
+
+	<div class="w3-container">
+		<div class="w3-container w3-card-4">
+			<br>
+
+			<b>Hallo [NAME], </b><br><br>
+
+			herzlichen Dank für deine Bestellung. <br>
+			Dein Auftrag ist bei uns um [UHRZEIT] eingegangen. <br>
+			<hr>
+
+			<b>Deine Bestellung: </b><br>
+
+			<table>
+				<tr>
+
+			    				
+			    				<th>Artikelname</th>
+			    				<th>Anzahl</th>
+			    				<th>Preis</th>
+			  			</tr>
+			</table>
+
+			<br><br>
+
+			Summe:			<br>
+			Versand:		<br><hr>
+			Gesamtsumme:	<br><hr>
+
+			Einen Guten Appetit, <br>
+			wünscht dir dein <b>MyPizza</b> Lieferdienst. <br>
+			Lass es dir schmecken! <br><br>
+		</div>
+	</div>
+</body>
+</html>
