4dbec76c80a47c22611e346eff5af439c0c6f41c
Liana2808 <40233721+Liana2808@users.noreply.github.com>
2019-01-10 16:01:16 +0100

    Create bestätigung.php

---
+++ bestätigung.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+	<title>Bestell Bestätigung</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<p>
+	<b>Hallo [NAME], </b><br><br>
+
+	herzlichen Dank für deine Bestellung. <br>
+	Dein Auftrag ist bei uns um [UHRZEIT] eingegangen. <br><hr>
+
+	<b>Deine Bestellung: </b><br>
+
+	<table>
+		<tr>
+
+	    				
+	    				<th>Artikelname</th>
+	    				<th>Anzahl</th>
+	    				<th>Preis</th>
+	  			</tr>
+	</table>
+
+	<br><br>
+
+	Summe:			<br>
+	Versand:		<br><hr>
+	Gesamtsumme:	<br><hr>
+
+	Lass es dir schmecken! <br>
+	Dein MyPizza Team.
+
+
+	
+</p>
+
+</body>
+</html>
