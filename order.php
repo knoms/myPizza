64c68d909abf41f1abdd359b37a2d0325fe0d7ef
Liana2808 <40233721+Liana2808@users.noreply.github.com>
2019-01-15 22:09:31 +0100

    Update order.php

--- order.php
+++ order.php
@@ -107,6 +107,52 @@ $OrderID = $resultOrderID['OrderID'];
     .mySlides {display:none;}
      body, a:hover, button:hover {cursor: url(pizza.cur), default;}
   </style>
+
+    <script type="text/javascript">
+    var URL = "php/UserOnline.txt";
+
+(function loadTxt() {
+  ajaxRequest(URL, function(xhr) {
+    document.getElementById('container').innerHTML = xhr.responseText;
+    setTimeout(loadTxt, 2000);
+  });
+})();
+
+function ajaxRequest(url, callback) {
+  var xhr;
+  if (typeof XMLHttpRequest !== 'undefined') xhr = new XMLHttpRequest();
+  else {
+    var versions = ["MSXML2.XmlHttp.5.0",
+      "MSXML2.XmlHttp.4.0",
+      "MSXML2.XmlHttp.3.0",
+      "MSXML2.XmlHttp.2.0",
+      "Microsoft.XmlHttp"
+    ]
+    for (var i = 0, len = versions.length; i < len; i++) {
+      try {
+        xhr = new ActiveXObject(versions[i]);
+        break;
+      } catch (e) {}
+    } 
+  }
+  xhr.onreadystatechange = ensureReadiness;
+  function ensureReadiness() {
+    if (xhr.readyState < 4) {
+      return;
+    }
+    if (xhr.status !== 200) {
+      return;
+    }
+    if (xhr.readyState === 4) {
+      callback(xhr);
+    }
+  }
+  xhr.open('GET', url, true);
+  xhr.send('');
+}
+</script>
+
+
 </head>
 <body>
     <!-- NAVIGATIONS BEREICH -->
@@ -331,6 +377,7 @@ $OrderID = $resultOrderID['OrderID'];
     <div class="w3-bar w3-light-green" style="">
       <a href="impressum.php" class="w3-bar-item w3-button">Impressum</a>
       <a href="kontaktformular.php" class="w3-bar-item w3-button">Kontaktformular</a>
+      <span class="w3-bar-item w3-right">User online: <span class="w3-tag" id="container">0</span> <p></span>
     </div>
 </footer>
 </body>
